cb3a5c6406842928c5b60b75b3d38fe3d9694ad7
ChikoHakles <2014484.chikopratama@gmail.com>
2023-03-03 10:20:52 +0700

    Add: CORS enable

--- src/main/java/com/chikohakles/customer/controllers/CustomerController.java
+++ src/main/java/com/chikohakles/customer/controllers/CustomerController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,9 +23,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //nambahin /api di url nya
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api")
 public class CustomerController {
-
     @Autowired
     //bikin variabel object customer repository (dari file CustomerRepository.java) (bentuknya List)
     private CustomerRepository customerRepository;
